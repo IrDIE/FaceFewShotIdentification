1946591ad4580277d798f6792f3eddfe9ee42845
IrDIE <110756720+IrDIE@users.noreply.github.com>
2024-08-31 19:30:06 +0300

    check another way to take webcam input

--- app.py
+++ app.py
@@ -26,3 +26,14 @@ c = webrtc_streamer(key="example2"
                     media_stream_constraints={"video": True, "audio": False},
 
                     )
+
+st.button("Add person")
+
+img_file_buffer = st.camera_input("Take a picture")
+
+if img_file_buffer is not None:
+    # To read image file buffer as bytes:
+    bytes_data = img_file_buffer.getvalue()
+    # Check the type of bytes_data:
+    # Should output: <class 'bytes'>
+    st.write(type(bytes_data))
